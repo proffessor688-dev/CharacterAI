2e31ea4646ca2c7c0bd911589a7c768790e5060f
proffessor688-dev <proffessor688@gmail.com>
2025-12-05 23:58:49 +0530

    Db connection establish

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConnection.js";
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT||5000;
 
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
 });
